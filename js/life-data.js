6b6b472aacfc1c0cf0c4eb598d77668a75ec0c1e
zzzzzmj <zzzzzmj@zzzzzmjdeMacBook-Air.local>
2026-09-18 13:34:49 +0800

    Add life journal notes and balanced thumbnails

--- js/life-data.js
+++ js/life-data.js
@@ -1,30 +1,30 @@
 window.LIFE_ENTRIES = [
   {
-    date: '2026-09-14',
+    date: '2026-09-13',
     location: '',
-    text: '',
+    text: '刚开学的9·13',
     photos: [
       {
-        src: '../images/life/2026/09/0914-01.jpg',
-        thumb: '../images/life/2026/09/thumbs/0914-01.jpg',
-        alt: '2026 年 9 月 14 日生活照片一'
+        src: '../images/life/2026/09/0913-01.jpg',
+        thumb: '../images/life/2026/09/thumbs/0913-01.jpg',
+        alt: '2026 年 9 月 13 日生活照片一'
       },
       {
-        src: '../images/life/2026/09/0914-02.jpg',
-        thumb: '../images/life/2026/09/thumbs/0914-02.jpg',
-        alt: '2026 年 9 月 14 日生活照片二'
+        src: '../images/life/2026/09/0913-02.jpg',
+        thumb: '../images/life/2026/09/thumbs/0913-02.jpg',
+        alt: '2026 年 9 月 13 日生活照片二'
       },
       {
-        src: '../images/life/2026/09/0914-03.jpg',
-        thumb: '../images/life/2026/09/thumbs/0914-03.jpg',
-        alt: '2026 年 9 月 14 日生活照片三'
+        src: '../images/life/2026/09/0913-03.jpg',
+        thumb: '../images/life/2026/09/thumbs/0913-03.jpg',
+        alt: '2026 年 9 月 13 日生活照片三'
       }
     ]
   },
   {
     date: '2026-07-10',
     location: '',
-    text: '',
+    text: '小周和小金～',
     photos: [
       {
         src: '../images/life/2026/07/0710-01.jpg',
@@ -36,7 +36,7 @@ window.LIFE_ENTRIES = [
   {
     date: '2026-07-07',
     location: '',
-    text: '',
+    text: '韩国网吧',
     photos: [{
       src: '../images/life/2026/07/0707-01.jpg',
       thumb: '../images/life/2026/07/thumbs/0707-01.jpg',
@@ -46,7 +46,7 @@ window.LIFE_ENTRIES = [
   {
     date: '2026-07-04',
     location: '',
-    text: '',
+    text: '韩国爬山',
     photos: [{
       src: '../images/life/2026/07/0704-01.jpg',
       thumb: '../images/life/2026/07/thumbs/0704-01.jpg',
@@ -56,7 +56,7 @@ window.LIFE_ENTRIES = [
   {
     date: '2026-07-02',
     location: '',
-    text: '',
+    text: '抵达韩国',
     photos: [
       {
         src: '../images/life/2026/07/0702-01.jpg',
